fda18a6f6147416850ba5d0849b1c1bc6ab94995
Akihiko Yamaguchi <info@akihikoy.net>
2023-12-08 13:30:28 +0900

    Decreased the sleep duration in streamingThread in IDLE state from 0.250 to 0.020 to improve the response;

--- motoman_driver/src/joint_trajectory_streamer.cpp
+++ motoman_driver/src/joint_trajectory_streamer.cpp
@@ -484,7 +484,7 @@ void MotomanJointTrajectoryStreamer::streamingThread()
     switch (this->state_)
     {
     case TransferStates::IDLE:
-      ros::Duration(0.250).sleep();  //  slower loop while waiting for new trajectory
+      ros::Duration(0.020).sleep();  //  slower loop while waiting for new trajectory
       break;
 
     case TransferStates::STREAMING:
